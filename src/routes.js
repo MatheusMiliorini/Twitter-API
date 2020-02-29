65bc46d5d081a456bb86f7e5d2bd6445bb22b6cc
Matheus Miliorini <miliorinimatheus@gmail.com>
2020-02-29 14:22:35 -0300

    Endpoints de RT

--- src/routes.js
+++ src/routes.js
@@ -214,4 +214,79 @@ routes.post("/unfav", async function (req, res) {
   });
 });
 
+// RT um tweet
+routes.post("/retweet", async function (req, res) {
+  const { _id, id } = req.body;
+
+  // Usuário não está logado
+  if (!_id)
+    return res.status(401).json({ error: "_id não informado!" });
+
+  // ID do Tweet
+  if (!id)
+    return res.status(401).json({ error: "id do tweet não informado!" });
+
+  const { oauth_token, oauth_token_secret } = await Usuario.findById(_id);
+
+  // Busca a Timeline
+  let request_data = {
+    url: `https://api.twitter.com/1.1/statuses/retweet/${id}.json`,
+    method: "POST",
+    data: {}
+  }
+  const tokens = {
+    key: oauth_token,
+    secret: oauth_token_secret
+  }
+
+  axios({
+    url: request_data.url,
+    method: request_data.method,
+    headers: oauth.toHeader(oauth.authorize(request_data, tokens)),
+    data: qs.stringify(request_data.data)
+  }).then(data => {
+    return res.send(data.data)
+  }).catch(e => {
+    res.status(500).send(e.message);
+  });
+});
+
+// Unretweet um tweet
+routes.post("/unretweet", async function (req, res) {
+  const { _id, id } = req.body;
+
+  // Usuário não está logado
+  if (!_id)
+    return res.status(401).json({ error: "_id não informado!" });
+
+  // ID do Tweet
+  if (!id)
+    return res.status(401).json({ error: "id do tweet não informado!" });
+
+  const { oauth_token, oauth_token_secret } = await Usuario.findById(_id);
+
+  // Busca a Timeline
+  let request_data = {
+    url: `https://api.twitter.com/1.1/statuses/unretweet/${id}.json`,
+    method: "POST",
+    data: {
+      id
+    }
+  }
+  const tokens = {
+    key: oauth_token,
+    secret: oauth_token_secret
+  }
+  axios({
+    url: request_data.url,
+    method: request_data.method,
+    headers: oauth.toHeader(oauth.authorize(request_data, tokens)),
+    data: qs.stringify(request_data.data)
+  }).then(data => {
+    return res.send(data.data)
+  }).catch(e => {
+    res.status(500).send(e.message);
+  });
+});
+
 module.exports = routes;
